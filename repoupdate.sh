dd5d75bb6811012997f823cc926a5581d9c9dfa9
Human1895 <amplespencer@protonmail.com>
2019-11-08 23:25:07 -0500

    add packages.bz2 n shit

--- repoupdate.sh
+++ repoupdate.sh
@@ -1,1 +1,11 @@
-./dpkg-scanpackages
+dpkg-scanpackages -m debs /dev/null > Packages
+rm Packages.gz Packages.bz2
+gzip -c9 Packages > Packages.gz
+bzip2 -c9 Packages > Packages.bz2
+
+read message 
+git add .
+git commit -m "$message"
+git push
+
+#copied from Dgh0st.github.io
